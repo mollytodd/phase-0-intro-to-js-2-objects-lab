e6155474ff4e0a32fd28997809b3905a97d05930
molly todd <mollyjt89@gmail.com>
2023-05-19 22:15:30 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee =  {
+    name: "Molly Todd",
+    address: "378 Paseo Marguerita",
+};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+    return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+function deleteFromEmployeeByKey (obj, key) {
+  
+const newObj = { ...obj };
+delete newObj[key];
+return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey (obj, key) {
+    delete obj[key];
+    return obj;
+}
