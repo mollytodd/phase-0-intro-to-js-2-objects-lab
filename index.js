2867a077999fa565b6814096f4737c5e3093911b
mollytodd <131923397+mollytodd@users.noreply.github.com>
2023-08-01 12:42:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-const employee =  {
-    name: "Molly Todd",
-    address: "378 Paseo Marguerita",
+const employee = {
+  name: "Molly",
+  streetAddress: "378 Paseo Marguerita",
 };
-function updateEmployeeWithKeyAndValue(obj, key, value) {
-    const newObj = { ...obj };
-    newObj[key] = value;
-    return newObj;
+
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+
+  const newArray = {
+   ...employee,
+   [key]: value,
+  };
+  return newArray;
 }
-function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value) {
-    obj[key] = value;
-    return obj;
+
+console.log(updateEmployeeWithKeyAndValue(employee, "streetAddress", "11 Broadway"));
+
+
+// this is working in the console but i'm not sure I am understanding the read my correctly
+//updated and fixed! line 12 needed to have the paremeters from the function, not a const!!
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee
 }
-function deleteFromEmployeeByKey (obj, key) {
-  
-const newObj = { ...obj };
-delete newObj[key];
-return newObj;
+
+destructivelyUpdateEmployeeWithKeyAndValue(
+  employee,
+  "streetAddress",
+  "12 Broadway"
+);
+console.log(employee)
+
+function deleteFromEmployeeByKey(employee,key){
+    const clonedEmployee = {...employee}
+    delete clonedEmployee[key];
+    return clonedEmployee;
 }
-function destructivelyDeleteFromEmployeeByKey (obj, key) {
-    delete obj[key];
-    return obj;
+const updatedEmployee = deleteFromEmployeeByKey("Molly", "StreetAddress")
+
+console.log(updatedEmployee)
+console.log(employee)
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+delete employee[key];
+return employee
 }
+destructivelyDeleteFromEmployeeByKey(employee, "streetAddress")
+console.log(employee)
